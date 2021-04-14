93441d4adb061408527b7cbcaf651edc67084d2c
Gabriel Hochstatter <gabrielhochstatter@Gabriels-MacBook-Pro-2.local>
2021-04-14 23:39:13 +0100

    update jest config

--- jest.config.js
+++ jest.config.js
@@ -5,7 +5,7 @@ module.exports = {
   transform: {
     "^.+\\.js$": "babel-jest",
   },
-  setupTestFrameworkScriptFile: "<rootDir>/enzyme.js",
+  setupFilesAfterEnv: ["<rootDir>/enzyme.js"],
   coverageReporters: ["json", "lcov", "text", "text-summary"],
   moduleNameMapper: {
     "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
